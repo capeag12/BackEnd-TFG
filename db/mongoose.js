7badca7637a38ae9ae1dd3e4b94b46c635ca15e1
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-03-07 13:17:04 +0100

    V1

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/bbddTFG')
+mongoose.connect('mongodb+srv://admin:admin@cluster0.hb3eci5.mongodb.net/gestor_almacenes')
