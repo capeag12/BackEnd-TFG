fbbd11049376c1456646b6f894971fc9651d2918
Carlos Pérez Aguilera <capeag12@gmail.com>
2023-05-04 20:38:04 +0200

    04-05-2023
    Ya genera pdfs, falta maquetar correctamente

--- routers/RouterMovimientos.js
+++ routers/RouterMovimientos.js
@@ -5,6 +5,8 @@ const AlmacenItem = require('../models/AlmacenItem');
 const Item = require('../models/Item');
 const Usuario = require('../models/Usuario');
 const Movimiento = require('../models/Movimiento');
+var pdf = require('pdf-creator-node');
+var fs = require('fs');
 
 const router = new express.Router();
 
@@ -50,6 +52,82 @@ router.get("/movimientos/getAllMovements", auth, async (req, res) => {
         });
     }
 
+    
+});
+
+router.get("/movimientos/getPDF/:id", auth, async (req, res) => {
+    try{
+        
+        let pdfLocation = process.cwd()+"\\pdfTemplates\\movementPDF.html"
+        var html = fs.readFileSync(pdfLocation, 'utf8');
+
+        let movimiento = await Movimiento.findById(req.params.id).populate('itemsDiferencia').populate('destino').populate('origen');
+
+        
+
+        let movItems = movimiento.itemsDiferencia
+        let itemsHTML = []
+        for (let i = 0; i < movItems.length; i++) {
+            let item = {
+                nombre: movItems[i].nombre,
+                valor: movItems[i].valor,
+                cantidad: movimiento.items[i].diferencia
+            }
+            console.log(item)
+            itemsHTML.push(item)
+            
+        }
+
+        let data = {
+            items: itemsHTML,
+            almacenOrigen: movimiento.origen[0].nombre,
+            almacenDestino: movimiento.destino[0].nombre,
+            fecha: movimiento.createdAt
+
+        }
+        console.log(data)
+
+        var options = {
+            format: "A4",
+            orientation: "portrait",
+            border: "10mm",
+            header:{
+                 height: "35mm",
+                 contents: `<div style="width: 80%; float:left;margin-left: 10%;margin-right: 10%;">
+                                <div style="width: 50%; float: left; text-align: start;">
+                                    <h5>Almacen origen:</h5>
+                                    <h4>${data.almacenOrigen}</h4>
+                                </div>
+                                <div style="width: 50%; float: right; text-align: end;">
+                                    <h5>Almacen destino:</h5>
+                                    <h4>${data.almacenDestino}</h4>
+                                </div>
+                            </div>`
+            },
+
+            footer: {
+                height: "30mm",
+                contents: {
+                    default:`<div class="footer">
+                                <p>Footer</p>
+                            </div>`
+                }
+            }
+        }
+
+        var document = {
+            html: html,
+            data:data ,
+            path: "./pdfTemplates/movementPDF.pdf",
+            type: "",
+        };
+
+        let pdfResult = await pdf.create(document, options)
+        console.log(pdfResult)
+    }
+    catch (error) {
+        console.log(error);
+    }
     
 });
 
